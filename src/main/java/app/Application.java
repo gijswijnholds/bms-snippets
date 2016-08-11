71b7a3a695d3336447f5a12d311eca537be17d0b
gijswijnholds <g.wijnholds@sig.eu>
2016-08-11 12:43:48 +0200

    altered the get request for the hello page

--- src/main/java/app/Application.java
+++ src/main/java/app/Application.java
@@ -3,21 +3,42 @@ package app;
 import static spark.Spark.get;
 import static spark.Spark.port;
 
-import app.snippet.SnippetDao;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import app.snippet.Snippet;
+import app.snippet.SnippetUrlDao;
+import spark.ModelAndView;
+import spark.Spark;
+import spark.template.velocity.VelocityTemplateEngine;
+
 
 public class Application {
 
-    public static SnippetDao snippetDao;
 
     public static void main(String[] args) {
-        snippetDao = new SnippetDao();
+
+        // SnippetFetcher fetcher = new SnippetFetcher();
+        Spark.staticFileLocation("/public");
 
         port(getHerokuAssignedPort());
-        
-        get("/hello",
-            (req, res) -> "This is the acceptance server. <br> Hello Sylvan and Zeeger, did you like this snippet: "
-            + snippetDao.getAllSnippets().iterator().next().getCode());
+        // get("/snippets/:chapter", );
+        get("/hello", (req, res) -> {
+            Map<String, Object> model = new HashMap<>();
+            model.put("intro", "Hello Sylvan and Zeeger, did you like this snippet: ");
+            SnippetUrlDao snippetUrlDao = new SnippetUrlDao();
+            Snippet snippet = snippetUrlDao.getSnippet(snippetUrlDao.getSnippetRefs().get(0));
 
+            String code = snippet.getCode();
+            List<String> codes = new ArrayList<String>();
+            codes.add(code);
+            codes.add(code);
+            model.put("codes", codes); //snippetUrlDao.getUrls()); // snippetDao.getAllSnippets().iterator().next().getCode());
+
+            return new ModelAndView(model, "/velocity/hello/test.vm");
+        }, new VelocityTemplateEngine());
     }
 
     static int getHerokuAssignedPort() {
@@ -27,4 +48,9 @@ public class Application {
         }
         return 4567; //return default port if heroku-port isn't set (i.e. on localhost)
     }
+
+    public String easterEgg() {
+        return "hallo";
+    }
+
 }
